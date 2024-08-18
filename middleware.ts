7d90013390d26f5693338b9a0a4ac304b7d2b0f7
danielluis07 <bombdaniel@hotmail.com>
2024-08-18 10:25:26 -0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,30 @@
-export { auth as middleware } from "@/auth";
+import NextAuth from "next-auth";
+
+import authConfig from "@/auth.config";
+import { publicRoutes } from "@/routes";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  // Redirect authenticated users from root URL to dashboard
+  if (isLoggedIn && nextUrl.pathname === "/") {
+    return Response.redirect(new URL("/", nextUrl));
+  }
+
+  // Redirect unauthenticated users trying to access protected routes
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/", nextUrl));
+  }
+
+  return undefined;
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
